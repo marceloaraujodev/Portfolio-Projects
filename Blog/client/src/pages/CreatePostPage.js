90709cfc0ad3f772e45e56e9bbc76dff297f47c6
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-02-29 16:45:35 -0300

    ss

--- Blog/client/src/pages/CreatePostPage.js
+++ Blog/client/src/pages/CreatePostPage.js
@@ -25,7 +25,7 @@ export default function CreatePost() {
     try {
       const response = await fetch(
         // 'http://localhost:4000/post', // development
-        'https://blog-rzyw.onrender.com/post', // production
+        'https://blog-rzyw.onrender.com/createPost', // production
         {
           method: 'POST',
           body: data,
