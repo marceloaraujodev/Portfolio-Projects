4cfcc179bc535127dce957465152af183ca4bf72
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-02-29 15:57:07 -0300

    adddd

--- Blog/api/server.js
+++ Blog/api/server.js
@@ -267,67 +267,67 @@ app.post('/logout', (req, res) => {
 // create post
 app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
   // app.post('/post',/* uploadMiddleware.single('file'), */ async (req, res) => {
-  // // development 👇 
-      const { originalname, path } = req.file;
-      const nameParts = originalname.split('.');
-      const ext = nameParts[nameParts.length - 1];
-      const newPath = path + '.' + ext;
-      fs.renameSync(path, newPath);
+  // // // development 👇 
+  //     const { originalname, path } = req.file;
+  //     const nameParts = originalname.split('.');
+  //     const ext = nameParts[nameParts.length - 1];
+  //     const newPath = path + '.' + ext;
+  //     fs.renameSync(path, newPath);
 
-    const { token } = req.cookies;
-    console.log(token)
-    jwt.verify(token, process.env.SECRET, async (err, info) => {
-      if (err) throw err;
-      const { title, summary, content, price } = req.body;
-      const newPost = await PostModel.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
-        price,
-        author: info.id,
-      });
-      res.status(200).json({
-        status: 'success',
-        newPost
-      });
-  });
-
-
-  // // production 👇
-  //   const { originalname, buffer } = req.file;
-  //   const { title, summary, content, price } = req.body;
-  //   console.log('pass 1')
   //   const { token } = req.cookies;
-  //   console.log('Token:', token);
-  //   console.log('pass 2')
+  //   console.log(token)
   //   jwt.verify(token, process.env.SECRET, async (err, info) => {
   //     if (err) throw err;
-  //     console.log('pass 3')
-  //     try {
-  //       const fileUploadOptions = {
-  //         destination: `covers/${originalname}`,
-  //         metadata: {
-  //           contentType: 'image/jpeg',
-  //         }
-  //       }
-  //       await bucket.upload(buffer, fileUploadOptions);
-  //       console.log('pass 4')
-  //       const newPost = await PostModel.create({
-  //         title,
-  //         summary,
-  //         content,
-  //         cover: newPath,
-  //         author: info.id,
-  //         price
-  //       });
-        
-  //       res.json(newPost);
-  //     } catch (error) {
-  //       console.error('Error uploading file:', error);
-  //       res.status(500).json('Internal server error');
-  //     }
+  //     const { title, summary, content, price } = req.body;
+  //     const newPost = await PostModel.create({
+  //       title,
+  //       summary,
+  //       content,
+  //       cover: newPath,
+  //       price,
+  //       author: info.id,
+  //     });
+  //     res.status(200).json({
+  //       status: 'success',
+  //       newPost
+  //     });
   // });
+
+
+  // production 👇
+    const { originalname, buffer } = req.file;
+    const { title, summary, content, price } = req.body;
+    console.log('pass 1')
+    const { token } = req.cookies;
+    console.log('Token:', token);
+    console.log('pass 2')
+    jwt.verify(token, process.env.SECRET, async (err, info) => {
+      if (err) throw err;
+      console.log('pass 3')
+      try {
+        const fileUploadOptions = {
+          destination: `covers/${originalname}`,
+          metadata: {
+            contentType: 'image/jpeg',
+          }
+        }
+        await bucket.upload(buffer, fileUploadOptions);
+        console.log('pass 4')
+        const newPost = await PostModel.create({
+          title,
+          summary,
+          content,
+          cover: newPath,
+          author: info.id,
+          price
+        });
+        
+        res.json(newPost);
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        res.status(500).json('Internal server error');
+      }
+  });
 })
 
 // Edit Post
