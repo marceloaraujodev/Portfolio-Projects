62750565dc35fd37e1f190ee3e0797122f3df6d3
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-01 11:42:51 -0300

    a

--- Blog/api/server.js
+++ Blog/api/server.js
@@ -231,49 +231,59 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     const { originalname, path } = req.file;
     const nameParts = originalname.split('.');
     const ext = nameParts[nameParts.length - 1];
-    const newPath = path + '.' + ext;
+    let newPath = null;
+    newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    console.log(token)
 
     jwt.verify(token, process.env.SECRET, async (err, info) => {
+      console.log('2')
       if (err) {
-        console.log('JWT verification failed:', err);
-        return res.status(401).json({ message: 'Unauthorized' });
+        console.log('JWT verification failed:', err)
+        return res.status(401).json({message: 'Unautohrized'})
+      }else{
+        console.log('Token verified')
+        console.log('3')
+        const { title, summary, content, price } = req.body;
+    
+        const newPost = await PostModel.create({
+          title,
+          summary,
+          content,
+          cover: req.file.path,
+          price,
+          author: info.id,
+        });
+        console.log('4')
+        const fileUploadOptions = {
+          destination: `covers/${originalname}`,
+          metadata: {
+            contentType: 'image/jpeg',
+          }
+        }
+    
+        const projectId = 'blog-storage-fb319';
+        const keyFilename = process.env.KEYFILENAME;
+    
+        const storage = new Storage({ projectId, keyFilename });
+    
+        const bucket = storage.bucket('blog-storage-fb319.appspot.com');
+        console.log('5')
+        await bucket.upload(req.file.path, fileUploadOptions);
+        console.log('6')
+        res.status(200).json({
+          status: 'success',
+          newPost,
+        });
+
       }
 
-      console.log('Token verified');
-      
-      const { title, summary, content, price } = req.body;
-
-      const newPost = await PostModel.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
-        price,
-        author: info.id,
-      });
-
-      const fileUploadOptions = {
-        destination: `covers/${originalname}`,
-        metadata: {
-          contentType: 'image/jpeg',
-        }
-      };
-
-      const projectId = 'blog-storage-fb319';
-      const keyFilename = process.env.KEYFILENAME;
-
-      const storage = new Storage({ projectId, keyFilename });
-
-      const bucket = storage.bucket('blog-storage-fb319.appspot.com');
-
-      await bucket.upload(req.file.path, fileUploadOptions);
-
-      res.status(200).json({
-        status: 'success',
-        newPost,
-      });
     });
+
+
+
   } catch (error) {
+    // error
     console.error('Error uploading file:', error);
     res.status(500).json('Internal server error');
   }
