0ac9d18c36730340a8d13f3867f5ffc67976df2f
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-01 13:41:52 -0300

    ua

--- Blog/client/src/pages/CreatePostPage.js
+++ Blog/client/src/pages/CreatePostPage.js
@@ -30,7 +30,7 @@ export default function CreatePost() {
           method: 'POST',
           body: data,
           credentials: 'include',
-          headers: { 'Content-Type': 'application/json' }
+          headers: { 'Content-Type': 'multipart/form-data' }
         }
         
       );
