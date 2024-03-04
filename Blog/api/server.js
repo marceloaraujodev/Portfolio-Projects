80cc4f8e94db7160794a2e3de3995fb805427a7c
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-04 17:24:50 -0300

    affff

--- Blog/api/server.js
+++ Blog/api/server.js
@@ -14,8 +14,6 @@ const admin = require('firebase-admin');
 const { Storage } = require('@google-cloud/storage');
 dotenv.config({ path: './config.env' });
 const stripe = require('stripe')(process.env.STIPE_SECRET_KEY);
-
-
 const corsOptions = {
   origin: ['https://summer-lab-1399.on.fleek.co', 'http://localhost:3000'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -28,8 +26,6 @@ const corsOptions = {
   ],
   credentials: true,
 };
-
-
 // multer, config limits for the post size!
 const uploadMiddleware = multer({
   dest: 'uploads/',
@@ -37,33 +33,24 @@ const uploadMiddleware = multer({
     fieldSize: 1024 * 1024 * 10, // 10MB limit for field size
   },
 });
-
 const app = express();
-
 app.use(morgan('dev')); // logger
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser()); // cookie parser
 app.use('/uploads', express.static(__dirname + '/uploads')); // serving all files from one
-
 //// WILL HAVE TO TURN ON DURING LOCAL TESTING
 // const db = process.env.DATABASE.replace(
 //   '<PASSWORD>',
 //   process.env.DATABASE_PASSWORD
 // );
-
 // DB connection // process.env.DATABASE
 mongoose.connect(process.env.DATABASE).then(() => console.log('Connected to Database'));
-
 // start server
 const PORT = 4000;
 const server = app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-
-
 app.get(`/checkout-session/:postId`, async (req, res) => {
   try {
     const { postId } = req.params;
@@ -92,7 +79,6 @@ app.get(`/checkout-session/:postId`, async (req, res) => {
       customer_email: 'ppzmarcelo@gmail.com', //req.user.email,
       client_reference_id: req.params.postId,
     });
-
     // create session  as response?!
     res.status(200).json({
       status: 'success',
@@ -106,16 +92,13 @@ app.get(`/checkout-session/:postId`, async (req, res) => {
 // Register user
 app.post('/register', async (req, res) => {
   const { username, password, email } = req.body;
-
   const encryptPass = await bcrypt.hash(password, 10);
-
   try {
     const userDoc = await UserModel.create({
       username: username,
       email: email,
       password: encryptPass,
     });
-
     res.status(200).json(userDoc);
   } catch (error) {
     console.log(error);
@@ -178,31 +161,30 @@ app.post('/logout', (req, res) => {
   res.cookie('token', '').json('logged out');
 });
 
-    // const { originalname, path } = req.file;
-    // const nameParts = originalname.split('.');
-    // const ext = nameParts[nameParts.length - 1];
-    // let newFileName = null;
-    // newFileName = path + '.' + ext;
-    // fs.renameSync(path, newFileName);
-
-    // path: 'uploads\\8c2a5829e6780fe7bd653775acf5ef71', = req.file.path
-
 async function bucketUpload(req){
   try {
-    console.log(req.file)
-
     const { originalname, path } = req.file;
     const nameParts = originalname.split('.');
     const ext = nameParts[nameParts.length - 1];
+    let newFileName = null;
+    newFileName = path + '.' + ext;
+    // // fs.renameSync(path, newFileName);
+
+    const fileUploadOptions = {
+      destination: `covers/` + path,
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    };
 
     console.log(req.file)
     const projectId = process.env.PROJECTID;
     const keyFilename = process.env.KEYFILENAME;
 
-        const metadata = { contentType: 'image/' + ext}
-        const storage = new Storage({ projectId, keyFilename });
-        const bucket = storage.bucket(process.env.BUCKET_NAME);
-
+    const metadata = { contentType: 'image/' + ext}
+    const storage = new Storage({ projectId, keyFilename });
+    const bucket = storage.bucket(process.env.BUCKET_NAME);
+    
     storage.getBuckets().then(x => console.log('Google Buckets:', x))
     
     await bucket.upload(path, {
@@ -214,28 +196,22 @@ async function bucketUpload(req){
           const publicUrl = `https://storage.googleapis.com/${process.env.BUCKET_NAME}/uploads/${req.file.filename + ext}`;
           console.log('Public URL:', publicUrl);
           return publicUrl;
-
   } catch (error) {
     console.error('Error uploading file:', error);
   }
 }
 
-
 // create post
 app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-
   if (!req.cookies.token) {
     return res.status(404).json({ message: 'No token found' });
   }
-
   jwt.verify(req.cookies.token, process.env.SECRET, async (err, info) => {
     if (err) {
       console.log('JWT verification failed:', err);
       return res.status(401).json({ message: 'Unautohrized: Invalid token' });
     }
-
     const { title, summary, content, price } = req.body;
-
     await PostModel.create({
       title,
       summary,
@@ -245,18 +221,14 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
       author: info.id,
     });
   });
-
   // upload files 
   const bucketRes = await bucketUpload(req)
-
   res.status(200).json({
     status: 'success',
     message: 'token verified confirmed',
     bucketData: bucketRes
   });
-
 });
-
 
 // Edit Post
 app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
@@ -268,12 +240,10 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
     newPath = path + '.' + ext;
     fs.renameSync(path, newPath);
   }
-
   const { token } = req.cookies;
   jwt.verify(token, process.env.SECRET, async (err, info) => {
     if (err) throw err;
     const { title, summary, content, id, price } = req.body;
-
     const dbPost = await PostModel.findById(id);
     const author = JSON.stringify(dbPost.author) === JSON.stringify(info.id);
     if (!author) {
@@ -314,7 +284,6 @@ app.get('/post/:id', async (req, res) => {
 app.delete('/post/:id', async (req, res) => {
   try {
     await PostModel.findByIdAndDelete(req.params.id);
-
     res.status(200).json({
       status: 'success',
       message: 'Post deleted successfully',
@@ -327,57 +296,19 @@ app.delete('/post/:id', async (req, res) => {
 server.on('close', () => {
   console.log('Server shutting down');
 });
-
-
-
-    // async function bucketUpload(req){
-    //   try {
-    //     const { originalname, path } = req.file;
-    //     const nameParts = originalname.split('.');
-    //     const ext = nameParts[nameParts.length - 1];
-    
-    //     const projectId = process.env.PROJECTID;
-    //     const keyFilename = process.env.KEYFILENAME;
-    //     console.log('req.file', req.file)
-    //     // console.log('New file name:', newFileName)
-    //     const metadata = { contentType: 'image/' + ext}
-    //     const storage = new Storage({ projectId, keyFilename });
-    //     const bucket = storage.bucket(process.env.BUCKET_NAME);
-    
-    //     await bucket.upload(path, {
-    //       destination: `uploads/${req.file.filename + ext}`, 
-    //       metadata: metadata,
-    //     });
-        
-    //     const publicUrl = `https://storage.googleapis.com/${process.env.BUCKET_NAME}/uploads/${req.file.filename + ext}`;
-    //     console.log('Public URL:', publicUrl);
-    
-    //     return publicUrl; // Or any other relevant data
-    
-    //   } catch (error) {
-    //     console.error('Error uploading file:', error);
-    //     throw error; 
-    //   }
-    // }
-
-
-
     // const fileUploadOptions = {
     //   destination: `uploads/` + req.file.originalname,
     //   metadata: {
     //     contentType: req.file.mimetype,
     //   },
     // };
-
     // console.log(req.file)
-
     // const { originalname, path } = req.file;
     // const nameParts = originalname.split('.');
     // const ext = nameParts[nameParts.length - 1];
     // let newFileName = null;
     // newFileName = path + '.' + ext;
     // fs.renameSync(path, newFileName);
-
         // console.log('this is Orinianl name:', originalname)
     // console.log('this is path:', path)
     // console.log('REQ.file:', req.file)
@@ -391,13 +322,8 @@ server.on('close', () => {
     // const upload = await storage
     // .bucket(process.env.BUCKET_NAME)
     // .upload(req.file.path + '.' + ext);
-
     // console.log(upload)
     // return upload
-
-
-
-
         // async function listBuckets() {
     //   const [buckets] = await storage.getBuckets();
     
