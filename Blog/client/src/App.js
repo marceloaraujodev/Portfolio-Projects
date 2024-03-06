7d8fae35fc1e05a2e615d23b0ac2a24292c1bd3e
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-06 17:55:02 -0300

    back

--- Blog/client/src/App.js
+++ Blog/client/src/App.js
@@ -12,17 +12,17 @@ import { UserContextProvider } from './components/UserContext';
 import Test from './components/Test';
 
 
-function App() {
+function App({queryClient}) {
   return (
     <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={'/login'} element={<LoginPage />} />
-        <Route path={'/register'} element={<RegisterPage />} />
-        <Route path={'/create'} element={<CreatePostPage />} />
-        <Route path={'/post/:id'} element={<PostPage />} />
-        <Route path={'/edit/:id'} element={<EditPostPage />} />
+        <Route path={'/login'} element={<LoginPage queryClient={queryClient} />} />
+        <Route path={'/register'} element={<RegisterPage queryClient={queryClient} />} />
+        <Route path={'/create'} element={<CreatePostPage queryClient={queryClient} />} />
+        <Route path={'/post/:id'} element={<PostPage queryClient={queryClient} />} />
+        <Route path={'/edit/:id'} element={<EditPostPage queryClient={queryClient} />} />
         <Route path={'/test'} element={<Test />} />
       </Route>
     </Routes>
