91244d83020fef6eb18fc88c7be2a20eace9653a
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-07 13:49:33 -0300

    afei

--- Blog/api/pages/login.jsx
+++ Blog/api/pages/login.jsx
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+  async function login(event) {
+
+    setIsLoggingIn(true);
 
 
-export default function Login(){
-  return <div>this is </div>
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      setIsLoggingIn(true);
+      alert('Wrong username or password');
+    }
+  }
+
+  return (
+    <form onSubmit={login} className="login">
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button disabled={isLoggingIn}>
+      {isLoggingIn ? 'Logging in...' : 'Login'}
+      </button>
+    </form>
+  );
 }
