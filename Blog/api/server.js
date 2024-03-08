e3bbca4aa39b32a48cd06433c0328f34f9178407
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-08 17:00:17 -0300

    af

--- Blog/api/server.js
+++ Blog/api/server.js
@@ -16,8 +16,8 @@ const admin = require('firebase-admin');
 dotenv.config({ path: './config.env' });
 const stripe = require('stripe')(process.env.STIPE_SECRET_KEY);
 const { v4: uuidv4 } = require('uuid');
-const serviceAccount = JSON.parse(process.env.KEYFIREBASE); // pro
-// const serviceAccount = require('./keyfirebase.json'); // dev
+// const serviceAccount = JSON.parse(process.env.KEYFIREBASE); // pro
+const serviceAccount = require('./keyfirebase.json'); // dev
 
 
 admin.initializeApp({
@@ -88,13 +88,7 @@ const server = app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('/login', (req, res) => {
-  res.status(200).render('login') // returns pug file
-})
 
-app.get('/register', (req, res) => {
-  res.render('register', { registerUser: { email: '', username: '', password: '' } });
-})
 
 app.get('/post', async (req, res) => {
   res.json(
