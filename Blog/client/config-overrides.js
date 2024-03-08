ca9a549bb45ebd4a949cd0e6b39f3f078ded9705
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-08 16:40:35 -0300

    a

--- Blog/client/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-  // Modify the Webpack configuration here...
-  config.devServer = {
-    ...config.devServer,
-    historyApiFallback: true,
-  };
-  return config;
-};
