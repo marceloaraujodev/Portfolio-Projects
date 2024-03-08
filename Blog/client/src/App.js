e6c13e913d49b45dd25f0bd9198bb843a7193fa1
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-08 00:04:04 -0300

    ar

--- Blog/client/src/App.js
+++ Blog/client/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path={'/login'} exact element={<LoginPage />} />
-        <Route path={'/register'}  element={<RegisterPage />} />
+        <Route path={'/register'} element={<RegisterPage />} />
         <Route path={'/create'} element={<CreatePostPage />} />
         <Route path={'/post/:id'} element={<PostPage />} />
         <Route path={'/edit/:id'} element={<EditPostPage />} />
